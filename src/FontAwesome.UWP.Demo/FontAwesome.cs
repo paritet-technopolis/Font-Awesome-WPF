b428c6a368a3c1b63c0acc784db4ceec911f5151
Robert Muehsig <robert.muehsig@codeinside.eu>
2016-01-28 00:13:31 +0100

    WIP UWP Support

---
+++ src/FontAwesome.UWP.Demo/FontAwesome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace FontAwesome.UWP.Demo
+{
+    public class FontAwesome : FontIcon
+    {
+        public FontAwesome()
+        {
+            this.FontFamily = new FontFamily("./fontawesome.otf#FontAwesome");
+            this.Glyph = "\uf17a";
+            
+            // this.FontFamily = new FontFamily("pack://application:,,,/FontAwesome.otf");
+        }
+
+        private FontAwesomeIcon _icon;
+        public FontAwesomeIcon Icon
+        {
+            get { return _icon; }
+            set
+            {
+                _icon = value;
+                // Glyph = "\f17a";
+            }
+        }
+    }
+}
